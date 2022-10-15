6596cf62f6b247c84287d84d17b605bec0e8d987
Metalit <48568911+Metalit@users.noreply.github.com>
2022-10-15 11:27:36 -0500

    Update rapidjson macros

--- include/Types/Config.hpp
+++ include/Types/Config.hpp
@@ -3,10 +3,10 @@
 #include "Types/Folder.hpp"
 
 DECLARE_JSON_CLASS(PlaylistManager, PlaylistConfig,
-    NAMED_AUTO_VALUE_DEFAULT(bool, Management, true, "enableManagement")
-    NAMED_AUTO_VALUE_DEFAULT(bool, DownloadIcon, false, "showDownloadIcon")
-    NAMED_AUTO_VALUE_DEFAULT(bool, RemoveMissing, true, "removeMissingBeatSaverSongs")
-    NAMED_AUTO_VECTOR(Folder, Folders, "folders")
+    NAMED_VALUE_DEFAULT(bool, Management, true, "enableManagement")
+    NAMED_VALUE_DEFAULT(bool, DownloadIcon, false, "showDownloadIcon")
+    NAMED_VALUE_DEFAULT(bool, RemoveMissing, true, "removeMissingBeatSaverSongs")
+    NAMED_VECTOR(Folder, Folders, "folders")
 )
 
 // defined in Main.cpp
--- include/Types/Folder.hpp
+++ include/Types/Folder.hpp
@@ -3,9 +3,9 @@
 #include "rapidjson-macros/shared/macros.hpp"
 
 DECLARE_JSON_CLASS(PlaylistManager, Folder,
-    NAMED_AUTO_VECTOR(std::string, Playlists, "playlists")
-    NAMED_AUTO_VALUE(std::string, FolderName, "folderName")
-    NAMED_AUTO_VALUE(bool, ShowDefaults, "showDefaults")
-    NAMED_AUTO_VECTOR(PlaylistManager::Folder, Subfolders, "subfolders")
-    NAMED_AUTO_VALUE(bool, HasSubfolders, "hasSubfolders")
+    NAMED_VECTOR(std::string, Playlists, "playlists")
+    NAMED_VALUE(std::string, FolderName, "folderName")
+    NAMED_VALUE(bool, ShowDefaults, "showDefaults")
+    NAMED_VECTOR(PlaylistManager::Folder, Subfolders, "subfolders")
+    NAMED_VALUE(bool, HasSubfolders, "hasSubfolders")
 )
