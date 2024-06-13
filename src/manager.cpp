8c6c27b17ec62e32d255606dbbc255f83f24bde2
Metalit <48568911+Metalit@users.noreply.github.com>
2024-06-13 16:26:22 -0500

    Fix exiting "add to playlist"

--- src/manager.cpp
+++ src/manager.cpp
@@ -69,6 +69,7 @@ namespace Manager {
     void OnMenuExit() {
         selectedPlaylist = nullptr;
         addingPlaylist = std::nullopt;
+        addingLevel = nullptr;
         if (shouldReload)
             Reload();
         if (auto nav = UnityEngine::Object::FindObjectOfType<GlobalNamespace::LevelFilteringNavigationController*>())
